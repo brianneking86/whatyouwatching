89d6b4bea61e0bc2eba6cc014ffaaa6f0bcfe490
brianneking86 <brianneking86@gmail.com>
2014-11-23 18:29:29 -0500

    updated show page styling and content

--- whatyouwatching/app/assets/javascripts/user.js
+++ whatyouwatching/app/assets/javascripts/user.js
@@ -4,31 +4,29 @@ $(document).ready(function(){
   $('#my_show_list').hide();
   $('#my_friends_list').hide();
 
-  $('.wrapper').on("click", "#sort_friends" ,function(){
+  $('.show_wrapper').on("click", "#sort_friends" ,function(){
     $('.sort_by_rating').hide();
     $('.sort_by_friends').show();
   });
 
-  $('.wrapper').on("click", '#sort_rating' ,function(){
+  $('.show_wrapper').on("click", '#sort_rating' ,function(){
     $('.sort_by_friends').hide();
     $('.sort_by_rating').show();
   });
 
-  $('.wrapper').on("click", '.show_title' ,function(e){
+  $('.show_wrapper').on("click", '.show_title' ,function(e){
     e.stopPropagation();
-    $(this).children('ul').toggle();
+    $(this).children('.show_info').toggle();
   });
 
-  $('.wrapper').on("click", '#my_shows' ,function(e){
+  $('.show_wrapper').on("click", '#my_shows' ,function(e){
     e.stopPropagation();
     $('#my_show_list').toggle();
   });
 
-  $('.wrapper').on("click", '#my_friends' ,function(e){
+  $('.show_wrapper').on("click", '#my_friends' ,function(e){
     e.stopPropagation();
     $('#my_friends_list').toggle();
   });
 
-  var ul = $('.list-inline')
-  ul.children().each(function(i,li){ul.prepend(li)})
 });
