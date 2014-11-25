ce78ed0b39fcb2d0386cd80b014927e22fce4cdd
brianneking86 <brianneking86@gmail.com>
2014-11-25 14:59:25 -0500

    final tweaks

--- whatyouwatching/app/controllers/user_shows_controller.rb
+++ whatyouwatching/app/controllers/user_shows_controller.rb
@@ -1,10 +1,11 @@
 class UserShowsController < ApplicationController
 
   def create
-    @user = User.find(session[:user_id])
+    @current_user = User.find(session[:user_id])
     @show_id = params[:user_show][:show_id]
-    us = UserShow.create(:user_id => @user.id, :show_id => @show_id)
-    redirect_to @user
+    us = UserShow.create(:user_id => @current_user.id, :show_id => @show_id)
+    redirect_to @current_user, notice: "#{Show.find(@show_id).title} has been added to your library!"
+    end
   end
 
 end
